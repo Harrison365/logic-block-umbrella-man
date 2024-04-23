95560bf9b0056b5333f92e1bb242cfd41a9d0d1f
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-04-23 16:14:26 +0100

    done

--- minUmbrellas.js
+++ minUmbrellas.js
@@ -1,2 +1,31 @@
-const minUmbrellas = (weatherArr) => {};
+const minUmbrellas = (weatherArr) => {
+  let count = 0;
+  let homeUmbrellas = 0;
+  let workUmbrellas = 0;
+  for (let i = 0; i < weatherArr.length; i++) {
+    if (i % 2 === 0) {
+      if (weatherArr[i] === "rainy" || weatherArr[i] === "thunderstorms") {
+        if (homeUmbrellas) {
+          homeUmbrellas--;
+          workUmbrellas++;
+        } else if (!homeUmbrellas) {
+          count++;
+          workUmbrellas++;
+        }
+      }
+    }
+    if (i % 2 === 1) {
+      if (weatherArr[i] === "rainy" || weatherArr[i] === "thunderstorms") {
+        if (workUmbrellas) {
+          workUmbrellas--;
+          homeUmbrellas++;
+        } else if (!workUmbrellas) {
+          count++;
+          homeUmbrellas++;
+        }
+      }
+    }
+  }
+  return count;
+};
 module.exports = minUmbrellas;
